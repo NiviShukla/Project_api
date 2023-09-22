781d44ad88f3e6978eb9941bb9b3f7793ea8e1f8
Nivesh Shukla <52473549+NiviShukla@users.noreply.github.com>
2023-09-22 10:48:06 +0530

    Delete apiproject directory

--- apiproject/apiproject/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Drink(models.Model):
-    name = models.CharField('Name', max_length=250)
-    description = models.CharField(max_length=500)
-    
-    def __str__(self):
-        return self.name
